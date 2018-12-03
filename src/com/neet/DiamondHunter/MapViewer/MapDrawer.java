2fcf24d21019e4c9898fba97f9542391ed34400e
joonwey <44994555+joonwey@users.noreply.github.com>
2018-12-03 23:21:45 +0800

    Added MapDrawer class. Import map.

---
+++ src/com/neet/DiamondHunter/MapViewer/MapDrawer.java
@@ -0,0 +1,54 @@
+package com.neet.DiamondHunter.MapViewer;
+
+import javafx.scene.canvas.GraphicsContext;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javafx.scene.image.Image;
+
+public class MapDrawer {
+    private int x;
+    private int y;
+    private int tileSize;
+    private int[][] map;
+    private int mapWidth;
+    private int mapHeight;
+    private Image tile;
+
+    public MapDrawer(String s, int tileSize) {
+        this.tileSize = tileSize;
+
+        try {
+            InputStream in = getClass().getResourceAsStream(s); //read map file
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            mapWidth = Integer.parseInt(br.readLine());
+            mapHeight = Integer.parseInt(br.readLine());
+            map = new int[mapHeight][mapWidth];
+
+            String delimiters = " ";
+            for (int row = 0; row < mapHeight; row++) {
+                String line = br.readLine();
+                String[] tokens = line.split(delimiters);
+                for (int col = 0; col < mapWidth; col++) {
+                    map[row][col] = Integer.parseInt(tokens[col]);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public void drawMap(GraphicsContext gc) { //draw map
+        tile = new Image("/Tilesets/testtileset.gif");
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map[row].length; col++) {
+                int sourcex = map[row][col];
+                int sourcey = 0;
+                if (sourcex >= (tile.getWidth() / tileSize)) {
+                    sourcey++;
+                    sourcex = (int) (sourcex - tile.getWidth() / tileSize);
+                }
+                gc.drawImage(tile, sourcex * tileSize, sourcey * tileSize, tileSize, tileSize, col * tileSize, row * tileSize, tileSize, tileSize);
+            }
+        }
+    }
+}
--- src/com/neet/DiamondHunter/MapViewer/MapViewer.java
+++ src/com/neet/DiamondHunter/MapViewer/MapViewer.java
@@ -1,12 +1,20 @@
 package com.neet.DiamondHunter.MapViewer;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.canvas.GraphicsContext;
+
 
 public class MapViewer extends Application {
+
+    @FXML
+    private Canvas canvas;
+    private MapDrawer tilemap;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -16,6 +24,14 @@ public class MapViewer extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 
+    }
+
+
+    public void initialize() {
+
+        tilemap = new MapDrawer("/Maps/testmap.map", 16);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        tilemap.drawMap(gc);
     }
 
     public static void main(String[] args){
